56734af52cbab83b8a7c609239dbc53a78e9a535
Russ Sayers <russell.sayers@gmail.com>
2020-07-19 17:29:02 +1000

    removed the ami hardcoding

--- instance.tf
+++ instance.tf
@@ -1,8 +1,17 @@
+data "aws_ami" "nginx-demo" {
+  most_recent      = true
+  owners           = ["self"]
 
-# resource "aws_instance" "web1" {
-#     ami = "ami-000e9db38e7ef9e37" // remove hardcoding
-#     instance_type = "t2.micro"
-#     # VPC
-#     subnet_id = aws_subnet.presentation-subnet-public-1.id
-#     vpc_security_group_ids = [ aws_security_group.web-open.id ]
-# }
+  filter {
+    name   = "name"
+    values = ["nginx-demo-*"]
+  }
+}
+
+resource "aws_instance" "web1" {
+    ami = data.aws_ami.nginx-demo.id
+    instance_type = "t2.micro"
+    # VPC
+    subnet_id = aws_subnet.presentation-subnet-public-1.id
+    vpc_security_group_ids = [ aws_security_group.web-open.id ]
+}

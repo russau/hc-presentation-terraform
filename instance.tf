1c94bd642ebd48892e009313516f1346e63091af
Russ Sayers <russell.sayers@gmail.com>
2020-07-19 17:09:52 +1000

    removing the instance for now

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 
-resource "aws_instance" "web1" {
-    ami = "ami-000e9db38e7ef9e37" // remove hardcoding
-    instance_type = "t2.micro"
-    # VPC
-    subnet_id = aws_subnet.presentation-subnet-public-1.id
-    vpc_security_group_ids = [ aws_security_group.web-open.id ]
-}
+# resource "aws_instance" "web1" {
+#     ami = "ami-000e9db38e7ef9e37" // remove hardcoding
+#     instance_type = "t2.micro"
+#     # VPC
+#     subnet_id = aws_subnet.presentation-subnet-public-1.id
+#     vpc_security_group_ids = [ aws_security_group.web-open.id ]
+# }

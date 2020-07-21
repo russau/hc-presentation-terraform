7356e38aa6bc9734d158dfa454e85e0fda120bf7
Russ Sayers <russell.sayers@gmail.com>
2020-07-21 13:03:40 +1000

    creating a role to decrypt SSL private key

--- certificate.tf
+++ certificate.tf
@@ -32,6 +32,42 @@ resource "aws_kms_ciphertext" "cert_private_key" {
   plaintext = acme_certificate.certificate.private_key_pem
 }
 
+# create an ec2 role
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# role can decrypt the private key
+resource "aws_iam_role" "instance_decrypt" {
+  name               = "instance_role"
+  assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "key_policy"
+  role = aws_iam_role.instance_decrypt.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "kms:Decrypt",
+        "Effect": "Allow",
+        "Resource": "${aws_kms_key.cert_key.arn}"
+      }
+    ]
+  }
+  EOF
+}
+
 # Output everything needed to configure nginix SSL config
 output "web_certificate_pem" {
   value = acme_certificate.certificate.certificate_pem
